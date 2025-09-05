5918f827cf6093a9b5eaf04f0db6ed8327504ca3
Joel Gotsch <joel.gotsch@gmail.com>
2025-09-05 02:00:30 +0200

    add tests

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -2,5 +2,7 @@ from turtle import end_fill
 from share_diffs.crypto import encrypt, decrypt
 
 def test_roundtrip():
-    test_str = "Hello World!"
-    assert decrypt(encrypt(test_str.encode())) == test_str
+    test_str = b"Hello World!"
+    assert decrypt(encrypt(test_str)) == test_str
+    long_teststr = test_str*1000
+    assert decrypt(encrypt(long_teststr)) == long_teststr
